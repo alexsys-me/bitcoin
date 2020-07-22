dee1f3bedf20ccbf8d2cf4c06ce98b4a832d1748
Alexsys <68325237+alexsys-me@users.noreply.github.com>
2020-07-22 15:30:57 +0300

    update

---
+++ build/js/main.js
@@ -0,0 +1,11 @@
+function animationHide() {
+
+    panel.style.transform = 'translateY(150%)';
+
+    setTimeout(function(){
+        panel.classList.remove('panel-controls_fixed'); 
+        panel.classList.remove('show');
+        panel.style.transform = '';
+    }, 400)
+}
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImZ1bmN0aW9uIGFuaW1hdGlvbkhpZGUoKSB7XHJcblxyXG4gICAgcGFuZWwuc3R5bGUudHJhbnNmb3JtID0gJ3RyYW5zbGF0ZVkoMTUwJSknO1xyXG5cclxuICAgIHNldFRpbWVvdXQoZnVuY3Rpb24oKXtcclxuICAgICAgICBwYW5lbC5jbGFzc0xpc3QucmVtb3ZlKCdwYW5lbC1jb250cm9sc19maXhlZCcpOyBcclxuICAgICAgICBwYW5lbC5jbGFzc0xpc3QucmVtb3ZlKCdzaG93Jyk7XHJcbiAgICAgICAgcGFuZWwuc3R5bGUudHJhbnNmb3JtID0gJyc7XHJcbiAgICB9LCA0MDApXHJcbn0iXSwiZmlsZSI6Im1haW4uanMifQ==
